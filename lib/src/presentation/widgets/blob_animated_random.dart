3117c963e110fe321c9bc87ba7beebf14d29dfe7
alaincrus06 <alaincruz06@gmail.com>
2022-09-26 17:07:31 -0400

    adding_hover_on_blobs

--- lib/src/presentation/widgets/blob_animated_random.dart
+++ lib/src/presentation/widgets/blob_animated_random.dart
@@ -1,6 +1,7 @@
+import 'dart:math';
+
 import 'package:blobs/blobs.dart';
 import 'package:flutter/material.dart';
-import 'package:portfolio/src/presentation/app/constants/on_hover.dart';
 import 'package:portfolio/src/presentation/app/theme/colors.dart';
 
 class BlobAnimatedRandom extends StatefulWidget {
@@ -12,27 +13,44 @@ class BlobAnimatedRandom extends StatefulWidget {
 
 class _BlobAnimatedRandomState extends State<BlobAnimatedRandom> {
   final BlobController blobCtrl = BlobController();
+  bool amIHovering = true;
+  double size = 200;
+  int edgesCount = 5;
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Blob.animatedRandom(
-      controller: blobCtrl,
-      size: 200,
-      loop: true,
-      edgesCount: 5,
-      minGrowth: 4,
-      duration: const Duration(milliseconds: 700),
-      styles: BlobStyles(
-        color: Colors.green,
-        fillType: BlobFillType.fill,
-        gradient: const RadialGradient(colors: [
-          PortfolioColors.primaryColor,
-          PortfolioColors.secondaryColor,
-          PortfolioColors.tertiaryColor
-        ]).createShader(
-          const Rect.fromLTRB(0, 0, 300, 300),
+    return MouseRegion(
+      onHover: (hovering) {
+        setState(() {
+          !amIHovering;
+          size = Random().nextDouble() * 250;
+          edgesCount = Random().nextInt(20);
+        });
+      },
+      child: Blob.animatedRandom(
+        controller: blobCtrl,
+        size: 200,
+        loop: amIHovering,
+        edgesCount: 5,
+        minGrowth: 4,
+        duration: const Duration(milliseconds: 700),
+        styles: BlobStyles(
+          color: Colors.green,
+          fillType: BlobFillType.fill,
+          gradient: const RadialGradient(colors: [
+            PortfolioColors.primaryColor,
+            PortfolioColors.secondaryColor,
+            PortfolioColors.tertiaryColor
+          ]).createShader(
+            const Rect.fromLTRB(0, 0, 300, 300),
+          ),
+          strokeWidth: 3,
         ),
-        strokeWidth: 3,
       ),
     );
   }
